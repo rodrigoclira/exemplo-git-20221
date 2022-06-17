234407bdc91f250df7a37fa99d2f5b99db753f23
Alderi Araújo <104461260+AlderiAraujo@users.noreply.github.com>
2022-06-17 09:58:11 -0300

    Update operacoes.py

--- calc/operacoes.py
+++ calc/operacoes.py
@@ -1,3 +1,14 @@
 #Arquivo inicial
 
 #Autor: Alderi
+
+
+def soma (num1,num2):
+    sub("Operação de Adição")
+    resultado = num1+num2
+    return resultado
+
+def subtração (num1,num2):
+    sub("Operação de Subtração")
+    resultado = num1-num2
+    return resultado
