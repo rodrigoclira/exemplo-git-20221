d881ea595ea8b69c68c705c242e9977bab67ef70
DevAugustomelo <99753415+DevAugustomelo@users.noreply.github.com>
2022-06-17 10:27:29 -0300

    Update operacoes.py
    Adicionado a função divisão e alterado os nomes das funções

--- calc/operacoes.py
+++ calc/operacoes.py
@@ -3,17 +3,25 @@
 #Autor: Alderi
 
 
-def soma (num1,num2):
+def soma(num1,num2):
     sub("Operação de Adição")
     resultado = num1+num2
     return resultado
 
 
-def subtracao (num1,num2):
+def sub(num1,num2):
     sub("Operação de Subtração")
     resultado = num1-num2
     return resultado
 
 
-def multiplica(num1, num2=2):
+def mult(num1=1, num2=2):
     return num1 * num2
+
+def div(num1=6, num2=6):
+    if num2 == 0:
+        return "Não é possível divisão por Zero"
+    else:
+        return num1 / num2
+    
+        
